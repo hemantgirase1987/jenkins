b9472a2d43bce3866180b30b7dc2007c2671db99
Hemant Girase <hemantgirase@Hemants-MacBook-Pro.local>
2020-09-06 20:44:03 +0530

    [master] | added script.groovy and Jenkinsfile_external_groovy

---
+++ script.groovy
@@ -0,0 +1,11 @@
+def buildFunction() {
+    echo "This is a build stage from master branch"
+}
+
+def testFunction() {
+    echo "This is a test stage from master branch"
+}
+
+def deployFunction() {
+    echo "This is a deploy stage from master branch"
+}
